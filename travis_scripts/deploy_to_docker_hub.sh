a7fd9b257c594d3daa10e9ee4cd3865a77daea32
kelleherke <kevin_kelleher@wcolwin1014600.education.gov.ie>
2017-08-21 17:04:56 +0100

    updating to reflect more accurate details

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,7 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker push kelleherke/tmx-addressservice:$BUILD_NAME
+docker push kelleherke/tmx-pupilservice:$BUILD_NAME
+docker push kelleherke/tmx-confsvr:$BUILD_NAME
+docker push kelleherke/tmx-eurekasvr:$BUILD_NAME
+docker push kelleherke/tmx-zuulsvr:$BUILD_NAME
