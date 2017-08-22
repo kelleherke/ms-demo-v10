e2de17e251ae95908a5094eaafeff4dce47de845
kelleherke <kevin_kelleher@wcolwin1014600.education.gov.ie>
2017-08-22 10:22:24 +0100

    adding scriting to push images to docker hub

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,7 +1,7 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push kelleherke/tmx-addressservice:$BUILD_NAME
-docker push kelleherke/tmx-pupilservice:$BUILD_NAME
-docker push kelleherke/tmx-confsvr:$BUILD_NAME
-docker push kelleherke/tmx-eurekasvr:$BUILD_NAME
-docker push kelleherke/tmx-zuulsvr:$BUILD_NAME
+docker push kelleherke/tmx-addressservice:v10
+docker push kelleherke/tmx-pupilservice:v10
+docker push kelleherke/tmx-confsvr:v10
+docker push kelleherke/tmx-eurekasvr:v10
+docker push kelleherke/tmx-zuulsvr:v10
