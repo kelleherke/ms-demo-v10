4347115945d7402302d43a207f3a98781329f92b
kelleherke <kevin_kelleher@wcolwin1014600.education.gov.ie>
2017-08-22 14:07:13 +0100

    again v2

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,5 +1,8 @@
-echo "Pushing citadel service docker images to docker hub ...."
+echo "Pushing kelleherke service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "See Images ....start"
+docker images
+echo "See Images ....end"
 docker push kelleherke/tmx-addressservice:v11
 docker push kelleherke/tmx-pupilservice:v11
 docker push kelleherke/tmx-confsvr:v11
