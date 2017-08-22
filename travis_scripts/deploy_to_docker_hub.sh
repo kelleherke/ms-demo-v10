45602a1c470c35b2c2dbfc7c1487d54d7e7d416f
kelleherke <kevin_kelleher@wcolwin1014600.education.gov.ie>
2017-08-22 14:43:49 +0100

    again v3

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -3,8 +3,8 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 echo "See Images ....start"
 docker images
 echo "See Images ....end"
-docker push kelleherke/tmx-addressservice:v11
-docker push kelleherke/tmx-pupilservice:v11
+docker push kelleherke/tmx-address-service:v11
+docker push kelleherke/tmx-pupil-service:v11
 docker push kelleherke/tmx-confsvr:v11
-docker push kelleherke/tmx-eurekasvr:v11
+docker push kelleherke/tmx-eureka-service:v11
 docker push kelleherke/tmx-zuulsvr:v11
