c647cda74eee32c3e22ee8aa8b8ac43d1074ee0c
kelleherke <kevin_kelleher@wcolwin1014600.education.gov.ie>
2017-08-22 10:32:53 +0100

    fixing script error

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,7 +1,7 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push kelleherke/tmx-addressservice:v10
-docker push kelleherke/tmx-pupilservice:v10
-docker push kelleherke/tmx-confsvr:v10
-docker push kelleherke/tmx-eurekasvr:v10
-docker push kelleherke/tmx-zuulsvr:v10
+docker push citadel/tmx-addressservice:v10
+docker push citadel/tmx-pupilservice:v10
+docker push citadel/tmx-confsvr:v10
+docker push citadel/tmx-eurekasvr:v10
+docker push citadel/tmx-zuulsvr:v10
